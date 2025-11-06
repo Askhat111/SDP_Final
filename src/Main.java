48be9ef1110864746fcddee0adbe384703b7e676
Askhat111 <240680@astanait.edu.kz>
2025-11-06 13:26:29 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Order order = new Order();
+        order.subscribe(new Customer());
+        order.subscribe(new Kitchen());
+
+        order.updateStatus("Ordered");
+
+        NewPayment payment = new SystemAdapter(new OldSystem());
+        payment.pay(20);
+
+        order.updateStatus("Ready");
     }
 }

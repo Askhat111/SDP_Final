24702868a6e87e6880b3aa73a4201c1502fa0a26
Askhat <askhat111@gmail.com>
2025-11-06 13:24:51 +0500

    projectt

---
+++ src/OldSystem.java
@@ -0,0 +1,9 @@
+class OldSystem { void oldPay(int cents) { System.out.println("Paid: " + cents + " cents"); } }
+
+interface NewPayment { void pay(double dollars); }
+
+class SystemAdapter implements NewPayment {
+    private OldSystem old;
+    public SystemAdapter(OldSystem old) { this.old = old; }
+    public void pay(double dollars) { old.oldPay((int)(dollars * 100)); }
+}

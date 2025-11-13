860c0d8c3f5a5fdb6582d3281f8c9bd5da8c4bf4
Askhat111 <240680@astanait.edu.kz>
2025-11-14 03:08:21 +0500

    Delete src directory

--- src/OldSystem.java
+++
@@ -1,9 +0,0 @@
-public class OldSystem { void oldPay(int cents) { System.out.println("Paid: " + cents + " cents"); } }
-
-interface NewPayment { void pay(double dollars); }
-
-class SystemAdapter implements NewPayment {
-    private OldSystem old;
-    public SystemAdapter(OldSystem old) { this.old = old; }
-    public void pay(double dollars) { old.oldPay((int)(dollars * 100)); }
-}
